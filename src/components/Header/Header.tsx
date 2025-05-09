dc4ca8ff3e6dd8824b030a3ed0ece5b755443dd7
mekaisen <mekaisenfapsgun@gmail.com>
2025-05-09 14:56:05 +0300

    modal

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { NavLink } from 'react-router';
-import { toast } from 'sonner';
 
 import { useTheme } from '../../utils/context/Theme/theme-provider';
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from '../ui/navigation-menu';
@@ -16,31 +15,9 @@ const Header = () => {
             <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
             <NavigationMenuContent>
               <ul className='flex p-4 gap-5 items-center'>
-                <li className=''>
-                  <NavLink to='/'>Home</NavLink>
-                </li>
-                <li className='flex items-center w-[80px] h-[80px] p-2 justify-center select-none rounded-md bg-gradient-to-b from-muted/50 to-muted no-underline outline-none focus:shadow-md'>
-                  <button
-                    type='button'
-                    onClick={() =>
-                      toast('Event has been created', {
-                        description: 'Sunday, December 03, 2023 at 9:00 AM',
-                        action: {
-                          label: 'Undo',
-                          onClick: () => console.log('Undo')
-                        },
-                        style: {
-                          border: '2px solid green'
-                        }
-                      })
-                    }
-                  >
-                    Show Toast
-                  </button>
-                </li>
-                <li><NavLink to='/auth'>Auth</NavLink></li>
+                <li className=''><NavLink to='/'>Home</NavLink></li>
+                <li className=''><NavLink to='/profile'>Profile</NavLink></li>
                 <li><button type='button' onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}> Toggle theme</button></li>
-                <li className='border-2 border-green-500 hidden'></li>
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
